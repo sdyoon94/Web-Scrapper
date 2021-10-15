97abbdd5dc1828812397dffd7f8ac64dbc236a4e
윤성도 <sdyoon94@gmail.com>
2021-10-15 11:09:10 +0900

    StackOverflow에서 구직정보 추출 완료

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from indeed import extract_indeed_pages, extract_indeed_jobs
+from indeed import get_jobs as get_indeed_jobs
+from stackoverflow import get_jobs as get_stackoverflow_jobs
 
-last_indeed_page = extract_indeed_pages()
-indeed_jobs = extract_indeed_jobs(last_indeed_page)
-print(indeed_jobs)
+indeed_jobs = get_indeed_jobs()
+stackoverflow_jobs = get_stackoverflow_jobs()
+jobs = indeed_jobs + stackoverflow_jobs
