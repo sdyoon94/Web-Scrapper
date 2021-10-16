35b5d42b658162def0bdb9171fea5a0d37787e98
윤성도 <sdyoon94@gmail.com>
2021-10-16 14:54:23 +0900

    fakeDB 만들기

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from stackoverflow import get_jobs as so_get_jobs
 
 app = Flask("SuperScrapper")
 
+db = {}
+
 @app.route("/")
 def home():
   return render_template("potato.html")
@@ -13,13 +15,16 @@ def report():
   word = request.args.get("word")
   if word:
     word = word.lower()
-    jobs=[]
-    jobs.append(i_get_jobs(word))
-    jobs.append(so_get_jobs(word))
-    print(jobs)
+    from_db = db.get(word)
+    if from_db:
+      jobs=from_db
+    else:
+      jobs=i_get_jobs(word)
+      jobs+=so_get_jobs(word)
+      db[word]=jobs
 
   else:
     return redirect("/")
 
-  return render_template("report.html", searching_by = word)
+  return render_template("report.html", searching_by = word, results_number = len(jobs))
 app.run(host="0.0.0.0")
